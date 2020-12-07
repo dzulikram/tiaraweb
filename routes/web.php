e8cd139aa0bb7ef839849a2039e8c04e83942b64
satriahelmy <helmysmp@gmail.com>
2020-12-08 07:53:38 +0800

    implementasi login dan logout

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Auth\LoginController@checkIsLogin');
 
 Route::get('maintenance', function () {
 	return view('maintenance');
@@ -23,27 +21,30 @@ Route::get('maintenance', function () {
 
 Auth::routes();
 
-Route::get('dashboard', 'PageController@index');
-Route::get('responden', 'PageController@responden');
+Route::group(['middleware' => ['auth']], function () {
+	Route::get('/logout', 'Auth\LoginController@logout');
+	Route::get('dashboard', 'PageController@index');
+	Route::get('responden', 'PageController@responden');
 
-Route::get('user', 'UserController@index');
-Route::get('user/create','UserController@create'); // create user
-Route::get('user/edit/{id}','UserController@edit'); //edit
-Route::post('user','UserController@store'); //store
-Route::put('user/{id}','UserController@update'); //update
-Route::get('user/delete/{id}','UserController@delete'); //delete
+	Route::get('user', 'UserController@index');
+	Route::get('user/create','UserController@create'); // create user
+	Route::get('user/edit/{id}','UserController@edit'); //edit
+	Route::post('user','UserController@store'); //store
+	Route::put('user/{id}','UserController@update'); //update
+	Route::get('user/delete/{id}','UserController@delete'); //delete
 
-Route::get('saran','SaranController@index'); // list saran
-Route::get('saran/create','SaranController@create'); // create saran
-Route::get('saran/edit/{id}','SaranController@edit'); //edit
-Route::post('saran','SaranController@store'); //store
-Route::put('saran/{id}','SaranController@update'); //update
+	Route::get('saran','SaranController@index'); // list saran
+	Route::get('saran/create','SaranController@create'); // create saran
+	Route::get('saran/edit/{id}','SaranController@edit'); //edit
+	Route::post('saran','SaranController@store'); //store
+	Route::put('saran/{id}','SaranController@update'); //update
 
-Route::get('chat','ChatController@index');
+	Route::get('chat','ChatController@index');
 
-Route::get('tiket','TiketController@index');
-Route::get('tiket-open','TiketController@indexOpen');
-Route::get('tiket-assigned','TiketController@indexAssigned');
-Route::get('tiket-resolved','TiketController@indexResolved');
-Route::get('assign-tiket/{id}','TiketController@assignTiket');
-Route::post('assign-tiket/{id}','TiketController@storeAssignTiket');
+	Route::get('tiket','TiketController@index');
+	Route::get('tiket-open','TiketController@indexOpen');
+	Route::get('tiket-assigned','TiketController@indexAssigned');
+	Route::get('tiket-resolved','TiketController@indexResolved');
+	Route::get('assign-tiket/{id}','TiketController@assignTiket');
+	Route::post('assign-tiket/{id}','TiketController@storeAssignTiket');
+});
