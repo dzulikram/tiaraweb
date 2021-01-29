f40b61b6865345fa647062e8c28d688373968b4f
satriahelmy <helmysmp@gmail.com>
2021-01-29 15:30:00 +0800

    update logo

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,6 +40,11 @@ class LoginController extends Controller
         //$this->middleware('guest')->except('logout');
     }
 
+    public function username()
+    {
+        return 'username';
+    }
+
     public function login(Request $request)
     {
         // check is expired
