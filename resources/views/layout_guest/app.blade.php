d740ad4fc1e2b0d312a1b74e342829cfa35b8580
dzulikram <dzulikram96@gmail.com>
2021-05-08 16:57:38 +0800

    migrasi16

--- resources/views/layout_guest/app.blade.php
+++ resources/views/layout_guest/app.blade.php
@@ -27,19 +27,7 @@
             <span class="navbar-toggler-icon icon-bar"></span>
           </button>
           <div class="collapse navbar-collapse justify-content-end">
-            <ul class="navbar-nav">
-              <li class="nav-item dropdown">
-                <a class="nav-link" href="javascript:;" id="navbarDropdownProfile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="material-icons">person</i>
-                  <p class="d-lg-none d-md-block">
-                    Account
-                  </p>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
-                  <a class="dropdown-item" href="{{url('/logout')}}">Log out</a>
-                </div>
-              </li>
-            </ul>
+            
           </div>
         </div>
       </nav>
--- resources/views/layout_guest/sidebar.blade.php
+++ resources/views/layout_guest/sidebar.blade.php
@@ -1,11 +1,11 @@
 <div class="sidebar-wrapper">
         <ul class="nav">
-          <!-- <li class="nav-item <?php if($state=='pojok'){echo 'active';} ?>">
+          <li class="nav-item <?php if($state=='pojok'){echo 'active';} ?>">
             <a class="nav-link" href="{{url('pojok-it')}}">
               <i class="material-icons">desktop_windows</i>
               <p>Pojok IT</p>
             </a>
-          </li> -->
+          </li>
           <li class="nav-item <?php if($state=='saran'){echo 'active';} ?>">
             <a class="nav-link" href="{{url('input-saran')}}">
               <i class="material-icons">chat</i>
